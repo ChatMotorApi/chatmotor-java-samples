9251f9074d39ad460d013766070023cee0a6141c
Nicolas de Pomereu <ndepomereu@kawansoft.com>
2024-07-19 18:53:36 +0200

    MotorTranscriptionRequestTest: add getDuration

--- src/main/java/com/chatmotorapi/samples/MotorTranscriptionRequestTest.java
+++ src/main/java/com/chatmotorapi/samples/MotorTranscriptionRequestTest.java
@@ -1,29 +1,3 @@
-/*
- * ChatMotor API Samples
- *
- * MIT License
- * 
- * Copyright (c) 2024 ChatMotorApi
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-
 package com.chatmotorapi.samples;
 
 import java.io.File;
@@ -45,14 +19,14 @@ public class MotorTranscriptionRequestTest {
 
 	String audioFilePath = SamplesParms.SAMPLE_FILES_DIR +  File.separator + "sunalsorises_01_hemingway_64kb.mp3";
 	String transcriptionFilePath = audioFilePath + ".txt";
-		
+	
 	System.out.println(new Date() + " Begin...");
 	//String model = "whisper-1";
 
 	// We assume that env var MOTOR_LICENSE_FILE_PATH and MOTOR_API_KEY are set
 	// Create a ChatMotor instance.
 	ChatMotor chatMotor = ChatMotor.builder().build();
-
+	
 	// Build the transcription request.
 	MotorTranscriptionRequest transcriptionRequest = MotorTranscriptionRequest.builder()
 		.chatMotor(chatMotor)
@@ -66,17 +40,18 @@ public class MotorTranscriptionRequestTest {
 	    try (InputStream in = largeReponse.getInputStream()) {
 		Files.copy(in, Paths.get(transcriptionFilePath), StandardCopyOption.REPLACE_EXISTING);
 	    }
-	 
+
 	    System.out.println("transcriptionFilePath:");
 	    System.out.println(transcriptionFilePath);
+
+	    if (!AndroidUtil.isAndroid()) {
+		int duration = MotorTranscriptionDuration.getDuration(chatMotor, new File(audioFilePath));
+		System.out.println("Duration in seconds: " + duration);
+	    }
+
 	} else {
-             System.out.println("throwable   : " + largeReponse.getThrowable());
+	    System.out.println("throwable   : " + largeReponse.getThrowable());
 	}
-	
-        if (! AndroidUtil.isAndroid()) {
-            int duration = MotorTranscriptionDuration.getDuration(chatMotor, new File(transcriptionFilePath));
-            System.out.println("Duration in seconds: " + duration);
-        }
 	
 	System.out.println(new Date() + " End.");
     }
