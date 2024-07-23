754dd33439159db0261923f76099bee8a415e4e1
Nicolas de Pomereu <ndepomereu@kawansoft.com>
2024-07-23 18:48:05 +0200

    Test classes

--- src/main/java/com/chatmotorapi/samples/MotorLargeTranscriptionRequestTest.java
+++ src/main/java/com/chatmotorapi/samples/MotorLargeTranscriptionRequestTest.java
@@ -1,3 +1,30 @@
+/*
+ * ChatMotor API Samples
+ *
+ * MIT License
+ * 
+ * Copyright (c) 2024 ChatMotorApi
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+
 package com.chatmotorapi.samples;
 
 import java.io.File;
@@ -9,11 +36,11 @@ import java.util.Date;
 
 import com.chatmotorapi.api.ChatMotor;
 import com.chatmotorapi.api.MotorLargeResponse;
+import com.chatmotorapi.api.transcription.MotorLargeTranscriptionRequest;
 import com.chatmotorapi.api.transcription.MotorTranscriptionDuration;
-import com.chatmotorapi.api.transcription.MotorTranscriptionRequest;
 import com.chatmotorapi.gpt.api.AndroidUtil;
 
-public class MotorTranscriptionRequestTest {
+public class MotorLargeTranscriptionRequestTest {
 
     public static void main(String[] args) throws Exception {
 
@@ -28,7 +55,7 @@ public class MotorTranscriptionRequestTest {
 	ChatMotor chatMotor = ChatMotor.builder().build();
 	
 	// Build the transcription request.
-	MotorTranscriptionRequest transcriptionRequest = MotorTranscriptionRequest.builder()
+	MotorLargeTranscriptionRequest transcriptionRequest = MotorLargeTranscriptionRequest.builder()
 		.chatMotor(chatMotor)
 		.filePath(audioFilePath)
 		.build();
