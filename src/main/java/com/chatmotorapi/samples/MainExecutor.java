58704b16253132d7bc966de4d837c84721b502f3
Nicolas de Pomereu <ndepomereu@kawansoft.com>
2024-07-24 17:08:37 +0200

    Audio conversion test not done on linux

--- src/main/java/com/chatmotorapi/samples/MainExecutor.java
+++ src/main/java/com/chatmotorapi/samples/MainExecutor.java
@@ -28,12 +28,16 @@ package com.chatmotorapi.samples;
 
 import java.util.Date;
 
+import org.apache.commons.lang3.SystemUtils;
+
 public class MainExecutor {
     public static void main(String[] args) throws Exception {
 
-	MotorAudioFileConverterTest.main(args);
-	System.out.println("*** " + new Date() + " MotorAudioFileConverterTest ***");
-	
+	if (SystemUtils.IS_OS_WINDOWS) {
+	    MotorAudioFileConverterTest.main(args);
+	    System.out.println("*** " + new Date() + " MotorAudioFileConverterTest ***");
+	}
+
 	MotorImageRequestSample.main(args);
 	System.out.println("*** " + new Date() + " MotorImageRequestSample ***");
 
